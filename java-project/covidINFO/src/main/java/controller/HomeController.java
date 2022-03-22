ad31defaae867d41a9c28895364b668e4e69b5b5
Sahil Khandelwal <sahilkhandelwal101@gmail.com>
2022-03-22 14:08:29 +0530

    added java project - partial done

---
+++ java-project/covidINFO/src/main/java/controller/HomeController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public String Home() {
+		System.out.println("this is home url");
+		return "index";
+	}
+}
