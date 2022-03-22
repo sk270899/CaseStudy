91bf0657c271c211817a17ec9bdc3e5cd2d5e315
Sahil Khandelwal <sahilkhandelwal101@gmail.com>
2022-03-23 02:10:15 +0530

    added covid-data-java project

--- java-project/covidINFO/src/main/java/controller/HomeController.java
+++ java-project/covidINFO/src/main/java/controller/HomeController.java
@@ -1,13 +1,17 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/home")
-	public String Home() {
-		System.out.println("this is home url");
-		return "index";
+	@RequestMapping("/submit")
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		return mv;
 	}
 }
