2713eb2a83ea79c9ac499463d06818078ad3fcc3
Sahil Khandelwal <sahilkhandelwal101@gmail.com>
2022-03-23 12:01:54 +0530

    added java-covid-project

---
+++ java-project/covidINFO-JDBC/src/main/java/com/covidJdbc/CovidJDBC/App.java
@@ -0,0 +1,34 @@
+package com.covidJdbc.CovidJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class App {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		String url = "jdbc:postgresql://localhost/student";
+		String username = "postgres";
+		String password = "postgres";
+		Class.forName("org.postgresql.Driver");
+
+		Connection conn = DriverManager.getConnection(url, username, password);
+
+		PreparedStatement stmt = conn.prepareStatement("SELECT location, " + "Count(total_cases ) FROM coviddata "
+				+ "WHERE new_cases> 1000 GROUP BY location,total_cases ORDER BY total_cases;");
+
+		ResultSet result = stmt.executeQuery();
+		while (result.next()) {
+			System.out.println(result.getString(1) + " " + result.getString(2)
+// + " " + result.getString(3)
+// + " " + result.getString(4)
+// + " " + result.getString(5)
+// + " " + result.getString(6)
+			);
+		}
+		conn.close();
+	}
+}
---
+++ java-project/covidINFO-MVC/src/main/webapp/index.jsp
@@ -0,0 +1,37 @@
+<html>
+<head>
+<style>
+body {background-color: white;
+	text-align:center;}
+h1   {color: black;
+	font-family:sans-serif;}
+p    {color: red;}
+h2	{font-family:sans-serif;}
+.button {
+  background-color: #c1d2d1; 
+  color: black; 
+  border: 2px solid #008CBA;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #008CBA;
+  color: white;
+}
+</style>
+</head>
+<body>
+<form action="submit">
+<h1>Covid Data Appication</h1>
+<h2>Hit Fetch to get the data</h2>
+<input class="button"type="submit" value="Fetch"/>
+</form>
+</body>
+</html>
